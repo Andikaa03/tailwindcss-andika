31187ff01bb9f6612ea8ce170c53c74ceef8d9e1
Andikaa03 <andikamr233@gmail.com>
2023-05-28 23:04:10 +0700

    alo

--- dist/js/index.js
+++ dist/js/index.js
@@ -2,19 +2,45 @@
 window.onscroll = function() {
     const header = document.querySelector('header');
     const navbar = header.offsetTop;
+    const top = document.querySelector('#top');
 
     if (window.pageYOffset > navbar) {
         header.classList.add('navbar-fixed');
+        top.classList.remove('hidden');
+        top.classList.add('flex');
     } else {
         header.classList.remove('navbar-fixed');
+        top.classList.remove('flex');
+        top.classList.add('hidden');
     }
 }
 
 // titik tigas
 const titik = document.querySelector('#titik');
+const navMenu = document.querySelector('#nav-menu');
 
-
-titik.addEventListener('click',function() {
+titik.addEventListener('click', function() {
     titik.classList.toggle('titik-active');
-    navMenu.classList.toggle('hidden')
+    navMenu.classList.toggle('hidden');
 });
+
+// Darkmode toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function() {
+    if (darkToggle.checked) {
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+    } else {
+        html.classList.remove('dark');
+        localStorage.theme = 'light';
+    }
+});
+
+// pindahkan posisi toggle sesuai mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+} else {
+    darkToggle.checked = false;
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
-  theme: {
-    container:{
-      center: true,
-      padding: '16px',
+    content: ["index.html"],
+    darkMode: 'class',
+    theme: {
+        container: {
+            center: true,
+            padding: '16px',
+        },
+        extend: {
+            colors: {
+                primary: '#4f46e5',
+                dark: '#0f172a',
+                second: '#64748b',
+            },
+            screens: {
+                '2xl': '1320px',
+            },
+        },
     },
-    extend: {
-      colors: {
-        primary: '#4f46e5',
-        dark: '#0f172a',
-        second: '#64748b',
-      },
-      screens: {
-        '2xl':'1320px',
-      },
-    },
-  },
-  plugins: [],
+    plugins: [],
 }
-
